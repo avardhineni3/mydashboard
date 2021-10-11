d079e859649817f0dca5f4219bd13756205b9c20
avardhineni3 <avardhineni3@gmail.com>
2021-10-11 08:02:41 +0530

    Azure codes

--- 6_AZURE_Terraform/azureBlob/variables.tf
+++ 6_AZURE_Terraform/azureBlob/variables.tf
@@ -1,24 +1,20 @@
 variable azure_subscription_id {
   type        = string
-  default     = ""
   description = "description"
 }
 
 variable azure_client_id {
   type        = string
-  default     = ""
   description = "description"
 }
 
 variable azure_client_secret {
   type        = string
-  default     = ""
   description = "description"
 }
 
 variable azure_tenant_id {
   type        = string
-  default     = ""
   description = "description"
 }
 
